b9b260a30b16205fd8040cce647764651771e9b4
PRAKASH RAJA <119003888+prakashraja-0723@users.noreply.github.com>
2024-02-03 09:01:30 +0530

    Update page.jsx

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -3,26 +3,25 @@ import React, { useState } from "react";
 import Navbar from "@/app/components/navbar/Navbar";
 import Cart from "@/app/cart/Cart";
 import Footer from "@/app/components/footer/footer";
-import Checkout from "@/app/cart/popup/checkout";
 
-function Page() {
+export default function Page() {
   const [isToggleModel, setIsToggleModel] = useState(false);
   console.log(isToggleModel);
-
+  
   return (
-    <main className={`w-screen  flex flex-col relative min-h-screen`}>
+    <main className={`w-screen flex flex-col relative min-h-screen ${isToggleModel && " h-screen overflow-hidden"}`}>
       <div
-        className={`bg-cart bg-cover min-h-[30vh] lg:h-[45vh] bg-center bg-no-repeat relative`}
+        className={`bg-cart bg-cover min-h-[30vh] lg:h-[45vh]  bg-center bg-no-repeat relative`}
       >
         <div
           className={`absolute top-1/2 -translate-x-1/2 left-1/2 -translate-y-1/2 lg:text-4xl font-bold uppercase text-white text-center text-xl w-full`}
         >
           <h1>Your Shopping Cart</h1>
         </div>
-        <Navbar />
+        <Navbar/>
       </div>
       <div
-        className={`my-4 lg:my-8 min-h-[66.5vh] lg:min-h-[50vh] flex items-start flex-col container mx-auto relative`}
+        className={`my-4 lg:my-8 min-h-[66.5vh] lg:min-h-[50vh] ${isToggleModel && "lg:h-[80vh] overflow-hidden"} flex items-start flex-col container mx-auto relative `}
       >
         <Cart isToggleModel={isToggleModel} setIsToggleModel={setIsToggleModel}/>
       </div>
@@ -30,5 +29,3 @@ function Page() {
     </main>
   );
 }
-
-export default Page;
